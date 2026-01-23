c3dadab6c0b4ad4fb87ba1ad1da21e0dd7e5fd44
Riley-Lin123 <rtlin94122@gmail.com>
2026-01-22 17:02:54 -0800

    Update PigLatin.java

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -1,45 +1,46 @@
 package org.APCSLowell;
 
 public class PigLatin {
+public int findFirstVowel(String sWord) {
+    for (int j = 0; j < sWord.length(); j++) {
+        if (sWord.substring(j, j + 1).equals("a") ||
+            sWord.substring(j, j + 1).equals("e") ||
+            sWord.substring(j, j + 1).equals("i") ||
+            sWord.substring(j, j + 1).equals("o") ||
+            sWord.substring(j, j + 1).equals("u")) {
 
-    public static int findFirstVowel(String sWord) {
-        for (int j = 0; j < sWord.length(); j++) {
-            String letter = sWord.substring(j, j + 1);
-            if (letter.equals("a") || letter.equals("e") ||
-                letter.equals("i") || letter.equals("o") ||
-                letter.equals("u")) {
-                return j;
-            }
+            return j;
         }
-        return -1;
     }
+    return -1;
+}
 
-    public static String pigLatin(String sWord) {
-        int position = findFirstVowel(sWord);
+public String pigLatin(String sWord) {
+    int position = findFirstVowel(sWord);
 
-        if (sWord.length() == 1) {
-            if (position == 0) {
-                return sWord + "way";
-            } else {
-                return sWord + "ay";
-            }
-        }
-
-        if (position == 1 &&
-            sWord.substring(0, 1).equals("q") &&
-            sWord.substring(1, 2).equals("u")) {
-            return sWord.substring(2) + "quay";
-        }
-
+    if (sWord.length() == 1) {
         if (position == 0) {
             return sWord + "way";
+        } else  {
+            return sWord + "ay";
         }
-
-        if (position > 0) {
-            return sWord.substring(position) +
-                   sWord.substring(0, position) + "ay";
-        }
-
-        return sWord + "ay"; // no vowels case
     }
+    if (position == 1 &&
+               sWord.substring(0, 1).equals("q") &&
+               sWord.substring(1, 2).equals("u")) {
+
+        return sWord.substring(2) + "quay";
+
+    } else if (position == 0) {
+        return sWord + "way";
+
+    } else if (position > 0) {
+        return sWord.substring(position)
+             + sWord.substring(0, position)
+             + "ay";
+
+    } else {
+        return sWord + "ay";
+    }
+}
 }
